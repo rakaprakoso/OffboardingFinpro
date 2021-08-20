18dd59c6258cad259b2794b2845cadea48f7a486
rakaprakoso <62010062+rakaprakoso@users.noreply.github.com>
2021-08-21 01:03:16 +0700

    Fix Title Head

--- resources/views/index_admin.blade.php
+++ resources/views/index_admin.blade.php
@@ -5,10 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Keina Beauty</title>
     <meta name="theme-color" content="#2a3f2b"/>
     <link rel="stylesheet" href="/css/app.css">
     <link rel="stylesheet" href="/css/appAdmin.css">
+    {!! SEO::generate() !!}
 </head>
 <body>
     <div id="root">
