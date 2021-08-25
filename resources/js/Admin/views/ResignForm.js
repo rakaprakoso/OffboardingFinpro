6a93e2a3fab22fc67823a78a4425ef93b2a0950d
rakaprakoso <62010062+rakaprakoso@users.noreply.github.com>
2021-08-25 23:07:40 +0700

    Fix post resign form

--- resources/js/Admin/views/ResignForm.js
+++ resources/js/Admin/views/ResignForm.js
@@ -28,15 +28,16 @@ const ResignForm = () => {
                         }}
                         onSubmit={async (values) => {
                             // console.log(values);
+                            const formData = new FormData();
                             // await new Promise((r) => setTimeout(r, 500));
                             // alert(JSON.stringify(values, null, 2));
-                            const formData = new FormData();
                             formData.append('employeeNameIn', values.employeeName);
                             formData.append('employeeIDIn', values.employeeID);
                             formData.append('reason', values.reason);
                             formData.append('effective_date', values.effectiveDate);
-                            formData.append('process_type',1);
-
+                            formData.append('process_type', 1);
+                            formData.append("resign_letter", values.resignLetter);
+                            console.log(formData);
                             // // Post the form, just make sure to set the 'Content-Type' header
                             const res = await axios.post('/api/resignform', formData, {
                                 headers: {
@@ -44,32 +45,34 @@ const ResignForm = () => {
                                 }
                             });
 
-                            // // Prints "yinyang.png"
                             console.log(res.data);
                         }}
                     >
-                        <Form>
-                            {/* <label htmlFor="employeeName">Employee Name</label>
+                        {(formProps) => (
+                            <Form>
+                                {/* <label htmlFor="employeeName">Employee Name</label>
                             <Field id="employeeName" name="employeeName" placeholder="Employee Name" /> */}
 
-                            <label htmlFor="employeeID">Employee ID</label>
-                            <Field id="employeeID" name="employeeID" placeholder="Employee ID" />
+                                <label htmlFor="employeeID">Employee ID</label>
+                                <Field id="employeeID" name="employeeID" placeholder="Employee ID" />
 
-                            <label htmlFor="reason">Resign Reason</label>
-                            <Field id="reason" type="textarea" name="reason" placeholder="Resign Reason" />
+                                <label htmlFor="reason">Resign Reason</label>
+                                <Field id="reason" type="textarea" name="reason" placeholder="Resign Reason" />
 
-                            <label htmlFor="effectiveDate">Effective Date</label>
-                            <input type="date" id="effectiveDate" name="effectiveDate"/>
+                                <label htmlFor="effectiveDate">Effective Date</label>
+                                <Field type="date" id="effectiveDate" name="effectiveDate" />
 
-                            <label htmlFor="file">Resign Letter</label>
-                            <input id="file" name="file" type="file" placeholder="Resign Reason"
-                                onChange={(event) => {
-                                    setFieldValue("file", event.currentTarget.files[0]);
-                                }}
-                            />
+                                <label htmlFor="resignLetter">Resign Letter</label>
+                                <input id="resignLetter" name="resignLetter" type="file" placeholder="Resign Letter"
+                                    onChange={(event) => {
+                                        formProps.setFieldValue("resignLetter", event.target.files[0]);
+                                    }}
+                                />
+                                <button type="submit">Submit</button>
+                            </Form>
 
-                            <button type="submit">Submit</button>
-                        </Form>
+
+                        )}
                     </Formik>
                 </div >
             </main>
